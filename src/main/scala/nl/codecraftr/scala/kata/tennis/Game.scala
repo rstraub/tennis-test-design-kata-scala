f2c9cbbbce54852d09eedc7ae1d1813c7c878d30
Roy Straub <roy@codecraftr.nl>
2023-02-08 21:26:43 +0100

    Pattern match with guard instead

--- src/main/scala/nl/codecraftr/scala/kata/tennis/Game.scala
+++ src/main/scala/nl/codecraftr/scala/kata/tennis/Game.scala
@@ -2,10 +2,12 @@ package nl.codecraftr.scala.kata.tennis
 
 case class Game(playerOnePoints: Int, playerTwoPoints: Int) {
   def toScore: Score = {
-    if (Winner.applies(this)) Winner(this)
-    else if (Deuce.applies(this)) Deuce(this)
-    else if (Advantage.applies(this)) Advantage(this)
-    else RunningScore(this)
+    this match {
+      case game if Winner.applies(game) => Winner(game)
+      case game if Deuce.applies(game) => Deuce(game)
+      case game if Advantage.applies(game) => Advantage(game)
+      case game => RunningScore(game)
+    }
   }
 
   def leadingPlayer =
--- src/main/scala/nl/codecraftr/scala/kata/tennis/RunningScore.scala
+++ src/main/scala/nl/codecraftr/scala/kata/tennis/RunningScore.scala
@@ -1,6 +1,11 @@
 package nl.codecraftr.scala.kata.tennis
 
 case class RunningScore(game: Game) extends Score {
+  private val LOVE = "love"
+  private val FIFTEEN = "fifteen"
+  private val THIRTY = "thirty"
+  private val FORTY = "forty"
+
   override def describe: String = {
     val playerOneScore = toScore(game.playerOnePoints)
     val playerTwoScore = toScore(game.playerTwoPoints)
@@ -11,10 +16,10 @@ case class RunningScore(game: Game) extends Score {
 
   private def toScore(points: Int) =
     points match {
-      case 0 => "love"
-      case 1 => "fifteen"
-      case 2 => "thirty"
-      case 3 => "forty"
+      case 0 => LOVE
+      case 1 => FIFTEEN
+      case 2 => THIRTY
+      case 3 => FORTY
     }
 }
 
