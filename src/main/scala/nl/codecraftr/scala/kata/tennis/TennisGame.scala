04e0d2aeae3019e01ade98943a7c5b3a42cb3e06
Roy Straub <roy@codecraftr.nl>
2023-02-08 17:43:17 +0100

    Made the code easier to understand

--- src/main/scala/nl/codecraftr/scala/kata/tennis/TennisGame.scala
+++ src/main/scala/nl/codecraftr/scala/kata/tennis/TennisGame.scala
@@ -2,21 +2,27 @@ package nl.codecraftr.scala.kata.tennis
 
 object TennisGame {
   def score(playerOnePoints: Int, playerTwoPoints: Int): String = {
-    val playerOneScore = toScore(playerOnePoints)
-    val playerTwoScore = toScore(playerTwoPoints)
+    if (isDeuce(playerOnePoints, playerTwoPoints)) "deuce"
 
-    if (playerOneScore == playerTwoScore) {
-      if (playerOnePoints < 3) s"$playerOneScore-all"
-      else "deuce"
-    } else s"$playerOneScore-$playerTwoScore"
+    else getRunningScore(playerOnePoints, playerTwoPoints)
   }
 
-  private def toScore(points: Int) =
-    points match {
-      case 0 => "love"
-      case 1 => "fifteen"
-      case 2 => "thirty"
-      case 3 => "forty"
-      case _ => "advantage"
-    }
+  private def isDeuce(playerOnePoints: Int, playerTwoPoints: Int) =
+    playerOnePoints >= 3 && playerTwoPoints >= 3 && playerOnePoints == playerTwoPoints
+
+  private def getRunningScore(playerOnePoints: Int, playerTwoPoints: Int) = {
+    def toScore(points: Int) =
+      points match {
+        case 0 => "love"
+        case 1 => "fifteen"
+        case 2 => "thirty"
+        case 3 => "forty"
+      }
+
+    val playerOneScore = toScore(playerOnePoints)
+
+    val playerTwoScore = toScore(playerTwoPoints)
+    if (playerOneScore == playerTwoScore) s"$playerOneScore-all"
+    else s"$playerOneScore-$playerTwoScore"
+  }
 }
