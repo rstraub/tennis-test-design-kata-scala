77d1bb4ba1fa1705a0450a0ac03cfcafc11814af
Roy Straub <roy@codecraftr.nl>
2023-02-08 15:48:56 +0100

    Implemented the first rule

--- src/main/scala/nl/codecraftr/scala/kata/tennis/TennisGame.scala
+++ src/main/scala/nl/codecraftr/scala/kata/tennis/TennisGame.scala
@@ -1,5 +1,15 @@
 package nl.codecraftr.scala.kata.tennis
 
 object TennisGame {
-    def score(playerOnePoints: Int, playerTwoPoints: Int): String = ""
+  def score(playerOnePoints: Int, playerTwoPoints: Int): String = {
+    s"${toScore(playerOnePoints)}-${toScore(playerTwoPoints)}"
+  }
+
+  private def toScore(points: Int) =
+      points match {
+          case 0 => "love"
+          case 1 => "fifteen"
+          case 2 => "thirty"
+          case 3 => "forty"
+      }
 }
