e6b3e2e2d31129e1f72c8a45584a83345247be19
Roy Straub <roy@codecraftr.nl>
2023-02-08 21:18:33 +0100

    Unindented method

--- src/main/scala/nl/codecraftr/scala/kata/tennis/RunningScore.scala
+++ src/main/scala/nl/codecraftr/scala/kata/tennis/RunningScore.scala
@@ -1,22 +1,21 @@
 package nl.codecraftr.scala.kata.tennis
 
-case class RunningScore(game: Game)
-    extends Score {
+case class RunningScore(game: Game) extends Score {
   override def describe: String = {
-    def toScore(points: Int) =
-      points match {
-        case 0 => "love"
-        case 1 => "fifteen"
-        case 2 => "thirty"
-        case 3 => "forty"
-      }
-
     val playerOneScore = toScore(game.playerOnePoints)
-
     val playerTwoScore = toScore(game.playerTwoPoints)
+
     if (playerOneScore == playerTwoScore) s"$playerOneScore-all"
     else s"$playerOneScore-$playerTwoScore"
   }
+
+  private def toScore(points: Int) =
+    points match {
+      case 0 => "love"
+      case 1 => "fifteen"
+      case 2 => "thirty"
+      case 3 => "forty"
+    }
 }
 
 case object RunningScore {
