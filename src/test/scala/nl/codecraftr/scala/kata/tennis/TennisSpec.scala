0414716eb7c969d03c0483a9f0bbd1f14b7ac1dd
Roy Straub <roy@codecraftr.nl>
2023-08-28 10:06:16 +0200

    Test outline

---
+++ src/test/scala/nl/codecraftr/scala/kata/tennis/TennisSpec.scala
@@ -0,0 +1,8 @@
+package nl.codecraftr.scala.kata.tennis
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class TennisSpec extends AnyFlatSpec with Matchers {
+  "score" should "do stuff" in {}
+}
